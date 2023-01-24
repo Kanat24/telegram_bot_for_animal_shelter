a517c8dc94bd90c51f1e6f68944b8d8c4e346a75
Sajer24 <sajer.kanat1992@gmail.com>
2023-01-25 00:06:10 +0300

    Первый коммит

---
+++ src/test/java/com/example/telegram_bot_for_animal_shelter/TelegramBotForAnimalShelterApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.telegram_bot_for_animal_shelter;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TelegramBotForAnimalShelterApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
