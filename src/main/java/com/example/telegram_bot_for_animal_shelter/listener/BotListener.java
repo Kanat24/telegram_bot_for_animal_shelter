1ee3f66e8101444b4efc1eb6ff9ec15840b343d8
ams32ru <ams32ru@mail.ru>
2023-01-28 19:53:18 +0300

    Создал listener

---
+++ src/main/java/com/example/telegram_bot_for_animal_shelter/listener/BotListener.java
@@ -0,0 +1,30 @@
+package com.example.telegram_bot_for_animal_shelter.listener;
+
+import com.pengrad.telegrambot.TelegramBot;
+import com.pengrad.telegrambot.UpdatesListener;
+import com.pengrad.telegrambot.model.Update;
+import jakarta.annotation.PostConstruct;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.beans.factory.annotation.Autowired;
+
+import java.util.List;
+
+public class BotListener implements UpdatesListener {
+
+    private final Logger logger = LoggerFactory.getLogger(BotListener.class);
+    @Autowired
+    private TelegramBot telegramBot;
+
+    @PostConstruct
+    public void init() {
+        telegramBot.setUpdatesListener(this);
+    }
+
+
+    @Override
+    public int process(List<Update> list) {
+
+        return CONFIRMED_UPDATES_ALL;
+    }
+}
