1c3e692426ad3f2959cbadb90d33d7bf1cf74934
ams32ru <ams32ru@mail.ru>
2023-01-28 19:59:30 +0300

    Научился реагировать на команду старт

--- src/main/java/com/example/telegram_bot_for_animal_shelter/listener/BotListener.java
+++ src/main/java/com/example/telegram_bot_for_animal_shelter/listener/BotListener.java
@@ -3,28 +3,40 @@ package com.example.telegram_bot_for_animal_shelter.listener;
 import com.pengrad.telegrambot.TelegramBot;
 import com.pengrad.telegrambot.UpdatesListener;
 import com.pengrad.telegrambot.model.Update;
+import com.pengrad.telegrambot.request.SendMessage;
 import jakarta.annotation.PostConstruct;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
-import org.springframework.beans.factory.annotation.Autowired;
 
 import java.util.List;
 
 public class BotListener implements UpdatesListener {
 
     private final Logger logger = LoggerFactory.getLogger(BotListener.class);
-    @Autowired
-    private TelegramBot telegramBot;
 
-    @PostConstruct
-    public void init() {
-        telegramBot.setUpdatesListener(this);
+    private  final TelegramBot telegramBot;
+
+    public BotListener(TelegramBot telegramBot) {
+        this.telegramBot = telegramBot;
+        this.telegramBot.setUpdatesListener(this);
     }
+
 
 
     @Override
     public int process(List<Update> list) {
-
+        list.stream()
+                .filter(update -> update.message() != null)
+                .forEach(this::processUpdate);
         return CONFIRMED_UPDATES_ALL;
+    }
+
+    private void processUpdate(Update update) {
+        String userMessage = update.message().text();
+        if (userMessage.equals("/start")) {
+            this.telegramBot.execute(new SendMessage(update.message().chat().id(),
+                    "Привет! я бот приюта для животных и тд"));
+        }
+
     }
 }
