eb0e1e66d922cd4e9e4fb1347866008d862703f7
ams32ru <ams32ru@mail.ru>
2023-01-29 16:38:59 +0300

    Добавил класс с константами

---
+++ src/main/java/com/example/telegram_bot_for_animal_shelter/configuration/BotConstants.java
@@ -0,0 +1,14 @@
+package com.example.telegram_bot_for_animal_shelter.configuration;
+
+public class BotConstants {
+
+    public static final String INITIAL_CMD = "/start";
+    public static final String GREETING_MSG = """
+            Привет!
+            Я - Бот приюта домашних животных! Если хочешь взять собаку или кошку домой я отвечу на твои вопросы,
+            так же можешь присылать мне отчеты о том как чувствуют себя наши животные!
+            Чем я могу  помочь?
+            """;
+
+
+}
